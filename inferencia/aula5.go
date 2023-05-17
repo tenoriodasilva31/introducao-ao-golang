a94435f49a73c200aba0d55eb622708384cc81c5
Jonathan Tenório <92552622+tenoriodasilva31@users.noreply.github.com>
2023-05-16 22:37:41 -0300

    Add files via upload

---
+++ inferencia/aula5.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nome string = "Jonathan"
+	var idade int = 29
+	var versao float32 = 5.2
+	fmt.Println("Meu nome é", nome, "e minha idade é", idade)
+	fmt.Println("Estou usando o programa de versão:", versao)
+}
